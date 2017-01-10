ab9eb213148c702f4165ba407fb1542431dd6d9d
seetwobyte <successscotty@gmail.com>
2017-01-09 23:19:43 -0500

    Aiden Chapter 6
    My Kid Python Lesson

---
+++ Aiden6.py
@@ -0,0 +1,19 @@
+Import turtle
+Import random
+Fred = turtle.Pen()
+fred.width(3)
+fred.speed(1)
+
+Def square(size)
+	For in range(4)
+	fred.forward(size)
+	fred.left(90)
+
+For i in range(100):
+	X = random.randrange(-200, 200)
+	Y = random.randrange(-200, 200)
+	fred.up()
+fred.goto(x, y)
+fred.down()
+Size - random.randrange(10, 200)
+Square(size)
