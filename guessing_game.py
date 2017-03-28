5961b8afeb46f7631b480f28ed6c6c945419b018
seetwobyte <successscotty@gmail.com>
2017-03-27 21:55:17 -0400

    last changes for Aiden Review

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,17 @@
 import random
 
 secret = random.randrange(1, 101)
-print(secret)
+guess = 0
+tries = 0
 
 while guess != secret:
-    guess = int(input)"Make a guess"
+    guess = int(input("Make a guess: "))
     tries = tries + 1
 
     if guess > secret:
         print("Too high!")
     elif guess < secret:
         print ("Too Low")
+    else:
+        print("You got it")
+print("Numbers of tries:", tries)
