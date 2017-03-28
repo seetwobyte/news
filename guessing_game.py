6598a9c30368f76a000384ceb394d8cf9deb6af4
seetwobyte <successscotty@gmail.com>
2017-03-27 21:51:06 -0400

    update2

--- guessing_game.py
+++ guessing_game.py
@@ -2,3 +2,12 @@ import random
 
 secret = random.randrange(1, 101)
 print(secret)
+
+while guess != secret:
+    guess = int(input)"Make a guess"
+    tries = tries + 1
+
+    if guess > secret:
+        print("Too high!")
+    elif guess < secret:
+        print ("Too Low")
